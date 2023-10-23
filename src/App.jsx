040521ff836452dae2476f7e7c6c242dbd3275c5
Isagani Esteron <isaganiesteron@ymail.com>
2023-10-23 22:10:05 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react"
-import { CssBaseline, Grid, Stack, Typography, Box, Button } from "@mui/material"
+import { CssBaseline, Grid, Stack, Typography, Box, Button, createTheme, ThemeProvider } from "@mui/material"
 import bgDesktop from "./assets/images/image-product-desktop.jpg"
 import bgMobile from "./assets/images/image-product-mobile.jpg"
 
+const theme = createTheme({
+	palette: {
+		background: { default: "hsl(30, 38%, 92%)" },
+	},
+})
+
 const App = () => {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<Grid container direction="row" xs={12}>
-				<Grid container item direction="colum" justifyContent="center" alignItems="center" sx={{ marginTop: "150px" }}>
-					<Grid container item direction="row" sx={{ width: "600px", height: "456.4px" }}>
-						<Grid item xs={12} md={6} sx={{ border: "1px solid black" }}>
-							<Box component="img" src={bgDesktop} width="300px" height="100%" />
+				<Grid container item direction="colum" justifyContent="center" alignItems="center" sx={{ marginTop: { xs: "50px", md: "150px" } }}>
+					<Grid container item direction="row" justifyContent="center" minWidth={"400px"} sx={{ width: { xs: "400px", md: "600px" }, height: { xs: "700px", md: "456.4px" } }}>
+						<Grid item xs={12} md={6} sx={{ border: "1px solid black", height: { xs: "275px", md: "425px" } }}>
+							<Box component="img" sx={{ content: { xs: `url(${bgMobile})`, md: `url(${bgDesktop})` }, width: { xs: "100%", md: "300px" }, height: "100%" }} />
 						</Grid>
-						<Grid container item xs={12} md={6} sx={{ padding: "10px 20px 10px 20px", border: "1px solid black" }}>
+						<Grid container item xs={12} md={6} sx={{ padding: "10px 20px 10px 20px", border: "1px solid black", height: { xs: "425px" } }}>
 							<Stack direction="column" justifyContent="space-evenly">
 								<Typography>PERFUME</Typography>
 								<Typography variant="h4">Gabriell Essence Eau De Parfum</Typography>
@@ -32,7 +38,7 @@ const App = () => {
 					</Grid>
 				</Grid>
 			</Grid>
-		</>
+		</ThemeProvider>
 	)
 }
 
